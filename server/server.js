25c2b08971b5d9fa77089fc48e3adf44cb3e8fdc
enkw <iankwolfe@gmail.com>
2024-05-03 01:04:31 -0400

    Updated server.js to include apollo server

--- server/server.js
+++ server/server.js
@@ -7,21 +7,38 @@ const path = require('path');
 // Uncomment this out when we get these files made
 // const { typeDefs, resolvers } = require('./');
 const db = require('./config/connection');
-const routes = require('./routes');
-
-const app = express();
+// Delete the commented out line below this when I'm certain I don't need it lol
+// const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+const startApolloServer = async () => {
+  await server.start();
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server));
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist'))); // Unsure if /dist is right here yet, was /build
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
